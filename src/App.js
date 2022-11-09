566d4de69c711bf2c2ee971b9e2194c2467cea54
Margaux Mengoni <margaux.mengoni@Gmail.com>
2022-11-09 11:28:41 +0000

    Ajout des text, boutns et pop-up avec les information sur les erreurs contrats

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import abi from './contracts/ABI.json';
 import { ethers } from 'ethers';
 
-const contractAddress = "0x9ebDb7acdCbCD6D5A15084246Ab3ab8231751d29";
+const contractAddressA = "0x9ebDb7acdCbCD6D5A15084246Ab3ab8231751d29";
+const contractAddressB = "";
+const contractAddressC = "";
 
 function App() {
 
@@ -46,7 +48,7 @@ function App() {
     }
   }
 
-  const mintNftHandler = async () => {
+  const mintNftHandler = async (contractAddress) => {
     try {
       const { ethereum } = window;
 
@@ -69,6 +71,7 @@ function App() {
 
     } catch (err) {
       console.log(err);
+      alert(err);
     }
   }
 
@@ -79,12 +82,32 @@ function App() {
       </button>
     )
   }
-
-  const mintNftButton = () => {
+              
+  const allNTFButton = () => {
     return (
-      <button onClick={mintNftHandler} className='cta-button mint-nft-button'>
-        Mint NFT
-      </button>
+      <div >
+        <h2>Contrat ouvert à tous</h2>
+        <p>
+          Ce contrat permet d'obtenir une NFT pokémon. <br/>
+          Il est utilisable 3 fois. Une fois cette limite atteinte, il n'est plus possible d'obtenir de NFT gratuite avec ce contrat. <br/>
+          Pour obtenir d'autre NFT pokémon, vous pouvez utiliser le contrat payant.
+        </p>
+        <button onClick={f => mintNftHandler(contractAddressA)} className='cta-button mint-nft-buttonA'>
+          Mint NFT 
+        </button>
+        <br/><br/>
+        <h2>Contrat payant</h2>
+        <p>Ce contrat permet d'obtenir une NFT pokémon pour la somme de 0.5 GoETH, sauf pour le owner pour qui c'est gratuit !</p>
+        <button onClick={f => mintNftHandler(contractAddressB)} className='cta-button mint-nft-buttonB'>
+          Mint NFT
+        </button>
+        <br/><br/>
+        <h2>Contrat disponible uniquement pour son propriétaire</h2>
+        <p>Ce contrat permet d'obtenir une NFT pokémon !</p>
+        <button onClick={f => mintNftHandler(contractAddressC)} className='cta-button mint-nft-buttonB'>
+          Mint NFT
+        </button>
+      </div>
     )
   }
 
@@ -94,9 +117,14 @@ function App() {
 
   return (
     <div className='main-app'>
-      <h1>TP ECE</h1>
+      <h1>Devoir maison Blockchain OCRES</h1>
+      <h3>Devoir réalisé par Magdeleine GAUVAIN et Margaux MENGONI </h3>
+      <p>
+        Cette app permet d'exécuter des smarts contract pour obtenir des NFT pokémon. <br/> 
+        Une fois connecté avec votre walet, il est possible de minter le contrat de votre choix grâce au différents boutons. <br/> <br/> 
+      </p>
       <div>
-        {currentAccount ? mintNftButton() : connectWalletButton()}
+          {currentAccount ? allNTFButton() : connectWalletButton()}
       </div>
     </div>
   )
